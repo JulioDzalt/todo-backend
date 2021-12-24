4455723e05c8edc2f47b79a8b44b88a08e3b348f
Julio C <julio.dzalt@gmail.com>
2021-12-23 21:34:22 -0600

    Added todod router in main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing_extensions import Concatenate
 import yaml
 from fastapi import FastAPI
+from todoBackend.controllers import TodoController
 
 config = "It's works"
 
@@ -10,7 +11,12 @@ with open(r'/home/julio/Desktop/todo-backend/config.yml') as file:
 
 app = FastAPI()
 
+app.include_router(TodoController.router)
 
 @app.get("/")
 async def root():
     return {"message": config["defaultMessage"]}
+
+@app.get("/version/")
+async def root():
+    return {"version": config["version"]}
