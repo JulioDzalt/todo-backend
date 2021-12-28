0febedc3bf0a06d706cf83778f0240548e516551
Julio C <julio.dzalt@gmail.com>
2021-12-27 22:26:16 -0600

    Updated bind script

--- bind_dev.sh
+++ bind_dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-docker run -it --name mini_api_container -p 90:90  \
+docker run \
+    -it \
+    --rm \
+    --name mini_api_container \
+    -p 90:90  \
     -v /home/julio/Desktop/todo-backend:/code \
-    mini_api /bin/bash
+    mini_api \
+    /bin/bash
