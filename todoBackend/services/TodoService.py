eb8d4d25e41a05c5ed69b17681ef293da70e60a0
Julio C <julio.dzalt@gmail.com>
2022-01-04 22:01:36 -0600

    optimized db conexion

--- todoBackend/services/TodoService.py
+++ todoBackend/services/TodoService.py
@@ -1,23 +1,27 @@
 from ..db.db import SessionLocal, engine
-
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
 from ..models import TodoModel as TodoModelFile
 from ..models.TodoModel import TodoModel
 
 TodoModelFile.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    try:
+        db = SessionLocal()
+        return db
+    finally:
+        db.close()
 
 class TodoService:
 
     # def __init__(self, id): #Constructor
     #     self.id = id
 
-    def getAllTodos(self):
-        db = SessionLocal()
+    def getAllTodos(self, db = get_db()):
         todos = db.query(TodoModel).all()
-        db.close()
         return todos
 
-    def getTodosById(self, todo_id):
-        db = SessionLocal()
-        todos = db.query(TodoModel).get(todo_id)
-        db.close()
+    def getTodosById(self, todo_id, db = get_db()):
+        todos = db.query(TodoModel).get(todo_id)    
         return todos
