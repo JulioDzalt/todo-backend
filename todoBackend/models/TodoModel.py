7a88ec1674926b683a202c30c35ccbbea75c3528
Julio C <julio.dzalt@gmail.com>
2022-01-06 20:57:32 -0600

    Update TodoModel, column definitions for sqlalchemy

--- todoBackend/models/TodoModel.py
+++ todoBackend/models/TodoModel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship
 
 from ..db.db import Base
@@ -8,8 +8,8 @@ class TodoModel(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
+    title = Column(String(255))
+    description = Column(Text)
 
     def __init__(self, title, description): #Constructor
         self.title = title
